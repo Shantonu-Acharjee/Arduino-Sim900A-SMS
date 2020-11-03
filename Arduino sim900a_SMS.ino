2c245642f42198a816c8564fa2844bb7a2ae933e
ShantonuAcharjee <54053134+ShantonuAcharjee@users.noreply.github.com>
2020-11-03 00:17:10 -0500

    Update Arduino sim900a_SMS.ino

--- Arduino sim900a_SMS.ino
+++ Arduino sim900a_SMS.ino
@@ -1,20 +1,34 @@
 #include <SoftwareSerial.h>
-SoftwareSerial gsm(7,8);//rx,tx
+SoftwareSerial gsm(7,8);//tx,rx
 String outMessage = "Distbin Full!";
-String destinationNumber = "+8801789333514";//01756473538  
+String destinationNumber = "+8801xxxxxxx";//Enter your phone number
 void setup()
 {
-  gsm.begin(4800);
-  gsm.print("AT+CMGF=1\r");
-  delay(1000);
-  gsm.println("AT + CMGS = \"" + destinationNumber +"\""); 
-  delay(1000);
-  gsm.print(outMessage);
-  delay(1000);
-  gsm.write((char)26);
-//ctrl+z 
+gsm.begin(115200);
+gsm.print("AT+CMGF=1\r");
 delay(1000);
-gsm.println("AT+CLTS=1"); 
+gsm.println("AT + CMGS = \"" + destinationNumber +"\"");
+delay(1000);
+gsm.print(outMessage);
+delay(1000);
+gsm.write((char)26);
+//ctrl+z
+delay(1000);
+gsm.println("AT+CLTS=1");
 gsm.println("AT+CCLK?");
-} 
+}
 void loop() { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
